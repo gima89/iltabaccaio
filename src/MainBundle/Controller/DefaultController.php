cb1c93e2d67a757204d29e58fd63f446c8055189
Giacomo Marolla <giacomo.marolla@gmail.com>
2016-05-12 18:22:34 +0200

    mappa funzionante - bootstrap da sistemare

--- src/MainBundle/Controller/DefaultController.php
+++ src/MainBundle/Controller/DefaultController.php
@@ -8,7 +8,27 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('MainBundle:Default:index.html.twig');
+        $tabaccherie = [
+            [
+                'name' => 'Tabaccheria 1',
+                'lat' => 45.34343,
+                'lng' => 9.5445,
+            ],
+            [
+                'name' => 'Tabaccheria 2',
+                'lat' => 45.3054343,
+                'lng' => 9.5345,
+            ],
+            [
+                'name' => 'Tabaccheria 3',
+                'lat' => 45.34243,
+                'lng' => 9.5465,
+            ],
+        ];
+        
+        return $this->render('MainBundle:Default:index.html.twig', [
+            'tabaccherie' => $tabaccherie,
+        ]);
     }
 
     public function resultsAction()
---
+++ web/js/script.js
@@ -0,0 +1,24 @@
+function initMap() {
+    var mapDiv = document.getElementById('map');
+    var map = new google.maps.Map(mapDiv, {
+        center: {lat: 45.466095, lng: 9.186005},
+        zoom: 12
+    });
+
+    tabaccherie.forEach(function (tabaccheria) {
+        var infowindow = new google.maps.InfoWindow({
+            content: '<h1>' + tabaccheria.name + '</h1>'
+        });
+        var marker = new google.maps.Marker({
+            position: {
+                lat: tabaccheria.lat,
+                lng: tabaccheria.lng
+            },
+            map: map,
+            title: tabaccheria.name
+        });
+        marker.addListener('click', function() {
+            infowindow.open(map, marker);
+        });
+    });
+}
